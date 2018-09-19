7e6495edae224327993ff4687e527151e2fe03db
gofunct <42948827+gofunct@users.noreply.github.com>
2018-09-18 22:27:16 -0600

    Delete install.sh

--- install.sh
+++
@@ -1,9 +0,0 @@
-echo "changing directory to Tasks"
-cd $GOPATH/src/github.com/thewhitetulip/Tasks
-echo "creating table"
-cat schema.sql | sqlite3 tasks.db
-echo "building the go binary"
-go build -o Tasks
-
-echo "starting the binary"
-./Tasks
